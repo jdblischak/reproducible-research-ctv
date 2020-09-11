b0a3626c5fb03754b781e3a234f239b307474d93
John Blischak <jdblischak@gmail.com>
2020-09-11 11:06:22 -0400

    Also check the date recorded in the ctv.

--- check.R
+++ check.R
@@ -19,6 +19,21 @@ xml <- xmlParse(ctvFile)
 urls = xpathSApply(xml, "//a[@href]", xmlGetAttr, "href")
 stopifnot(url.exists(urls))
 
+message("Checking date...")
+today <- Sys.Date()
+timezone <- Sys.timezone()
+versionNode <- getNodeSet(xml, "//version")
+versionValue <- xmlValue(versionNode)
+version <- as.Date(versionValue)
+if (version != today)
+  warning(
+    "\n",
+    sprintf("It is %s in the timezone %s\n", today, timezone),
+    sprintf("The version in ctv is %s\n", version),
+    "Don't forget to update the version\n",
+    immediate. = TRUE
+  )
+
 message("Building HTML and opening for viewing")
 ctv2html(ctvFile, file = htmlFile)
 browseURL(htmlFile)
